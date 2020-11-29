f630fd8a19d39fd138ef89ea197ef60c0d836cf8
KuneDeva <53827543+deva9017@users.noreply.github.com>
2020-11-29 15:31:34 +0530

    update my contents
    twilio sid and token

--- whatsapp_messaging.py
+++ whatsapp_messaging.py
@@ -3,18 +3,18 @@ from twilio.rest import Client
 def msg_mom_and_dad(event=None, context=None):
 
     # get your sid and auth token from twilio
-    twilio_sid = 'AC84c9f1602d7fb6af4eda5b0c39a03b37'
-    auth_token = '4a2021b28f1aa606d9c6945d3c248ebd'
+    twilio_sid = 'ACf95fb72cd0f797915cef66a0ef7e5768'
+    auth_token = '71174d069b34e4756371e2d08c811de6'
 
     whatsapp_client = Client(twilio_sid, auth_token)
 
     # keep adding contacts to this dict to send
     # them the message
-    contact_directory = {'daddy':'+919624666836'}
+    contact_directory = {'daddy':'+919700939598'}
 
     for key, value in contact_directory.items():
         msg_loved_ones = whatsapp_client.messages.create(
-                body = 'good morning {} !'.format(key),
+                body = 'good evng {} !'.format(key),
                 from_= 'whatsapp:+14155238886',
                 to='whatsapp:' + value,
 
